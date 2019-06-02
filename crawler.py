29a41e2e02c4ff8b24d9b9072959b764d4eaa04c
Aaron Rosenberg <aaron.matthew.rosenberg@gmail.com>
2019-06-01 19:29:19 -0600

    now resolving URLs from main page

--- crawler.py
+++ crawler.py
@@ -1,5 +1,14 @@
+import logging
+
 import lxml.html as lh
 import requests
+
+
+def main():
+    url = 'http://www.espn.com/'
+    anchors = get_anchor_elements(url)
+    resolved_urls = generate_links(url, anchors)
+    print(list(resolved_urls))
 
 
 def get_html(url):
@@ -11,13 +20,25 @@ def parse_html(html):
     """Returns lxml HTML root"""
     return lh.fromstring(html)
 
-def find_links(url, link_css='a'):
+
+def get_anchor_elements(url, link_css='a'):
     html = get_html(url)
     root = parse_html(html)
-    elements = root.cssselect(link_css)
-    return elements
+    for element in root.cssselect(link_css):
+        yield element
+
+
+def generate_links(url, anchors):
+        for anchor in anchors:
+            try:
+                href = anchor.attrib['href']
+                if href.startswith('http'):
+                    yield href
+                else:
+                    yield url + href
+            except:
+                logging.debug('no href, skipping anchor element ... ')
 
 
 if __name__ == '__main__':
-    url = 'http://www.espn.com/'
-    links = find_links(url)
+    main()
